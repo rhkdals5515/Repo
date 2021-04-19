63ad47d16f8d791c3d3ed06cd42ac1720a66a32d
K M Ahn <rhkdals3461@naver.com>
2021-04-19 14:48:41 +0900

    branchA에 추가

--- GitStudy/src/exam/Qustion2.java
+++ GitStudy/src/exam/Qustion2.java
@@ -5,6 +5,8 @@ public class Qustion2 {
 	public static void main(String[] args) {
 		System.out.println(Math.random());
 		
-		System.out.println("Have a good");
+		System.out.println("Random!!");
+		
+		System.out.println("branchA¿¡ Ãß°¡");
 	}
 }
