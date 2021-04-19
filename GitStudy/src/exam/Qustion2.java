e308ef9a465ea1ad2c7225d122b3934d70462c90
K M Ahn <rhkdals3461@naver.com>
2021-04-19 14:47:22 +0900

    branchB에 추가

--- GitStudy/src/exam/Qustion2.java
+++ GitStudy/src/exam/Qustion2.java
@@ -5,6 +5,11 @@ public class Qustion2 {
 	public static void main(String[] args) {
 		System.out.println(Math.random());
 		
-		System.out.println("Have a good");
+		System.out.println("Random!!");
+		
+		System.out.println("branchB에서 추가");
+		// branchA,B 모두에 커밋을 2개 추가하고 history 내역을 보낼 것
+		// (단, Question2만 수정해야 함 ※ 이 주석은 branchB에서 작성되었음)
+		
 	}
 }
